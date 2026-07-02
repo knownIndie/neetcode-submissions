df35affe3584ac81e745bd4ef74ad629575fef08
Aryan_Bhardwaj <bhardwaj03aryan@gmail.com>
2026-07-02 19:03:57 +0530

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        seen = {}
+        for val in s:
+            seen[val] = seen.get(val, 0) + 1
+        for val in t:
+            if val not in seen:
+                return False
+            else:
+                seen[val]-=1
+            
+            if seen[val] < 0:
+                return False
+
+        for count in seen.values():
+            if count != 0:
+                return False
+
+        return True
